76e4e2f70a5a10bb42dc524fb1a95020e8722e1f
Artur Pigula <arturpigula@gmail.com>
2021-12-02 16:17:24 +0100

    Added basic App.Metrics configuration

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,7 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var services = builder.Services;
+    services.AddMetrics()
+    .AddMetricsEndpoints()
+    .AddMetricsReportingHostedService()
+    .AddMetricsTrackingMiddleware();
+
 var app = builder.Build();
 
+app.UseMetricsAllMiddleware();
+
+app.UseMetricsAllEndpoints();
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/error", () => { throw new Exception("error"); });
 
 app.Run();
