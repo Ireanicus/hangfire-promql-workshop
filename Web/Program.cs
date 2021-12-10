5b1f4a4d9b5e2974b5b056c94d1fe33e32e70484
Artur Pigula <arturpigula@gmail.com>
2021-12-10 12:22:13 +0100

    Added example metrics

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,16 +1,19 @@
 using App.Metrics;
+using App.Metrics.Counter;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
-    services
-        .AddMetrics(options =>
-        {
-            options.OutputMetrics.AsPrometheusPlainText();
-        })
-        .AddMetricsEndpoints()
-        .AddMetricsReportingHostedService()
-        .AddMetricsTrackingMiddleware();
+services
+    .AddMetrics(options =>
+    {
+        options.OutputMetrics.AsPrometheusPlainText();
+    })
+    .AddMetricsEndpoints()
+    .AddMetricsReportingHostedService()
+    .AddMetricsTrackingMiddleware();
+
+services.AddHostedService<LabelMetricHostedService>();
 
 var app = builder.Build();
 
@@ -19,5 +22,20 @@ app.UseMetricsAllMiddleware();
 app.UseMetricsAllEndpoints();
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/error", () => { throw new Exception("error"); });
+app.MapGet("/customMetric", context =>
+{
+    var metrics = context.RequestServices.GetRequiredService<IMetrics>();
+
+    metrics.Measure.Counter.Increment(new CounterOptions
+    {
+        Name = "counter_example",
+        Tags = MetricTags.Concat(
+            new MetricTags("key", "value"),
+            new MetricTags("path", "customMetric"))
+    });
+
+    context.Response.StatusCode = 200;
+    return Task.CompletedTask;
+});
 
 app.Run();
