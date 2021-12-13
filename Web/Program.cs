b5dd15e50cdb3ee38e409064934526022d83361f
Artur <arturpigula@gmail.com>
2021-12-13 10:40:23 +0100

    added simple metric

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,9 +1,14 @@
 using App.Metrics;
+using App.Metrics.Counter;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 
-services.AddMetrics()
+services
+    .AddMetrics(options =>
+    {
+        options.OutputMetrics.AsPrometheusPlainText();
+    })
     .AddMetricsEndpoints()
     .AddMetricsReportingHostedService()
     .AddMetricsTrackingMiddleware();
@@ -16,5 +21,20 @@ app.UseMetricsAllMiddleware();
 app.UseMetricsAllEndpoints();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/simple", context =>
+{
+    var metrics = context.RequestServices.GetRequiredService<IMetrics>();
+    metrics.Measure.Counter.Increment(new CounterOptions
+    {
+        Name = "simple_metric",
+        MeasurementUnit = Unit.None,
+        //Tags = new MetricTags(new[] { "key1", "key2" }, new[] { "value1", "value2" })
+        Tags = MetricTags.Concat(new MetricTags("key1", "value1"),
+         new MetricTags("key2", Random.Shared.NextInt64().ToString()))
+    });
+
+    context.Response.StatusCode = 200;
+    return Task.CompletedTask;
+});
 
 app.Run();
