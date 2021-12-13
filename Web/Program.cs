8db94b80ad5ffec5e32bb4dc6457359a5b13e3e7
Artur <arturpigula@gmail.com>
2021-12-13 09:55:16 +0100

    added metrics

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,8 +1,19 @@
+using App.Metrics;
+
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+services.AddMetrics()
+    .AddMetricsEndpoints()
+    .AddMetricsReportingHostedService()
+    .AddMetricsTrackingMiddleware();
 
 var app = builder.Build();
 
 
+
+app.UseMetricsAllMiddleware();
+app.UseMetricsAllEndpoints();
 
 app.MapGet("/", () => "Hello World!");
 
